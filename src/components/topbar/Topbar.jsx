908b6bfb9e3d86ce5dd5e3ac3eec6d8b23615499
22415327-scu <chloeprofessionaldevelopment@outlook.com>
2024-04-09 16:49:02 +1000

    Added snap scrolling

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,24 @@
 import "./topbar.scss"
+import {Face3} from "@material-ui-icons"
 
 export default function Topbar() {
   return (
     <div className="topbar">
-      sdfsdfdfs
+     <div className = "wrapper">
+     <div className = "left">
+      <a href="#intro" className="logo">genuis.</a>
+      <div className="itemContainer">
+
+      <Face3/>
+      <span> dsfsdfsdf </span>
+      </div>
+      
+      </div>
+
+
+<div className="right"></div>
+
+     </div>
       
     </div>
   )
