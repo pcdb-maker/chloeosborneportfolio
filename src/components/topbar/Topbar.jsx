16a976d94d8fcbca7930d8202a3752b4954966b8
22415327-scu <chloeprofessionaldevelopment@outlook.com>
2024-04-09 17:49:05 +1000

    Added Mui icons

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,15 +1,14 @@
 import "./topbar.scss"
-import {Face3} from "@material-ui-icons"
+
 
 export default function Topbar() {
   return (
     <div className="topbar">
      <div className = "wrapper">
      <div className = "left">
-      <a href="#intro" className="logo">genuis.</a>
+      <a href="#intro" className="logo">Chloe O.</a>
       <div className="itemContainer">
 
-      <Face3/>
       <span> dsfsdfsdf </span>
       </div>
       
