13c9c1bc59379243f5bdbc9912fd90900ce691af
22415327-scu <chloeprofessionaldevelopment@outlook.com>
2024-04-11 14:04:28 +1000

    Animations added to burger menu

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,14 +3,12 @@ import { FaLocationArrow, FaMailBulk } from "react-icons/fa";
 
 
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar active ">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">Chloe O.</a>
-
-
 
           <div className="itemContainer">
             <FaLocationArrow className="icon" />
@@ -25,13 +23,11 @@ export default function Topbar() {
           </div>
 
 
-
-
         </div>
 
 
         <div className="right">
-          <div className="hamburger">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
             <span className="line1"></span>
             <span className="line2"></span>
             <span className="line3"></span>
