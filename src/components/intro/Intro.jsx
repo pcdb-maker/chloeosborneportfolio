4dd3cd6efb54e8d9d9acd74c766e14e9570a9451
22415327-scu <chloeprofessionaldevelopment@outlook.com>
2024-04-12 09:44:12 +1000

    Added transparent images to profile

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,10 +4,10 @@ import "./intro.scss"
 export default function Intro() {
   return (
     <div className="intro" id="intro"> 
-        <div className="left"></div>
-            <div className="imgContainer">
-              <img src='assests/me.png'></img>
-               </div>
+        <div className="left">
+          <div className="imgContainer"></div>
+          <img src="assests/me_code.png" alt="Cartoon version of me" />
+        </div>
         <div className="right"></div>
         
         </div>
