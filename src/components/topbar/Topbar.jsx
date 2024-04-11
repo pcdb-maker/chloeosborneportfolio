2ad1e6a1a9a973ac1c1eeb08b573fe808f7b8beb
22415327-scu <chloeprofessionaldevelopment@outlook.com>
2024-04-11 12:55:40 +1000

    Improved formatting

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -6,41 +6,41 @@ import { FaLocationArrow, FaMailBulk } from "react-icons/fa";
 export default function Topbar() {
   return (
     <div className="topbar active ">
-     <div className = "wrapper">
-     <div className = "left">
-      <a href="#intro" className="logo">Chloe O.</a>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">Chloe O.</a>
 
 
-      
-      <div className="itemContainer">
-        <FaLocationArrow  className="icon"/>
-       
-        <span>Location: Melbourne, VIC</span>
+
+          <div className="itemContainer">
+            <FaLocationArrow className="icon" />
+
+            <span>Location: Melbourne, VIC</span>
+          </div>
+
+          <div className="itemContainer">
+            <FaMailBulk className="icon" />
+
+            <span>Location: Melbourne, VIC</span>
+          </div>
+
+
+
+
+        </div>
+
+
+        <div className="right">
+          <div className="hamburger">
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+
+          </div>
+        </div>
+
       </div>
 
-      <div className="itemContainer">
-        <FaMailBulk   className="icon"/>
-       
-        <span>Location: Melbourne, VIC</span>
-      </div>
-  
-       
-      
-      
-      </div>
-
-
-<div className="right">
-  <div className="hamburger">
-<span className="line1"></span>
-<span className="line2"></span>
-<span className="line3"></span>
-
-  </div>
-</div>
-
-     </div>
-      
     </div>
   )
 }
