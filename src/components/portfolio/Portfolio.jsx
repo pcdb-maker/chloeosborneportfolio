7310aad62eaee277d2b809a3f525774e2adec310
22415327-scu <chloeprofessionaldevelopment@outlook.com>
2024-04-12 15:55:15 +1000

    Indenting and formatting

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,52 +4,52 @@ import "./portfolio.scss"
 export default function Portfolio() {
   return (
     <div className='portfolio' id="portfolio">
-    
-    <h1>Portfolio</h1>
-    <ul>
-      <li className="active">Web Applications</li>
-      <li>Mobile Applications</li>
-      <li>Digital Presence Optimixation</li>
-      <li>Graphic Design</li>
-      <li>Digital Art</li>
-      <li>C# Game Design (Unity)</li>
-    </ul>
+
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Web Applications</li>
+        <li>Mobile Applications</li>
+        <li>Digital Presence Optimixation</li>
+        <li>Graphic Design</li>
+        <li>Digital Art</li>
+        <li>C# Game Design (Unity)</li>
+      </ul>
 
 
-    <div className="container">
-      <div className="item">
-        <img src="\assests\down.png" alt=''/>
-        <h3>Banking App</h3>
+      <div className="container">
+        <div className="item">
+          <img src="\assests\down.png" alt='' />
+          <h3>Banking App</h3>
+        </div>
+
+        <div className="item">
+          <img src="\assests\down.png" alt='' />
+          <h3>Banking App</h3>
+        </div>
+
+        <div className="item">
+          <img src="\assests\down.png" alt='' />
+          <h3>Banking App</h3>
+        </div>
+
+        <div className="item">
+          <img src="\assests\down.png" alt='' />
+          <h3>Banking App</h3>
+        </div>
+
+        <div className="item">
+          <img src="\assests\down.png" alt='' />
+          <h3>Banking App</h3>
+        </div>
+
+        <div className="item">
+          <img src="\assests\down.png" alt='' />
+          <h3>Banking App</h3>
+        </div>
+
+
       </div>
 
-      <div className="item">
-        <img src="\assests\down.png" alt=''/>
-        <h3>Banking App</h3>
-      </div>
-
-      <div className="item">
-        <img src="\assests\down.png" alt=''/>
-        <h3>Banking App</h3>
-      </div>
-
-      <div className="item">
-        <img src="\assests\down.png" alt=''/>
-        <h3>Banking App</h3>
-      </div>
-
-      <div className="item">
-        <img src="\assests\down.png" alt=''/>
-        <h3>Banking App</h3>
-      </div>
-
-      <div className="item">
-        <img src="\assests\down.png" alt=''/>
-        <h3>Banking App</h3>
-      </div>
-
-      
-   </div>
-      
     </div>
   )
 }
