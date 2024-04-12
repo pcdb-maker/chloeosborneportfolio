f604f74bac89a259dd487eb483b08f6812657be9
22415327-scu <chloeprofessionaldevelopment@outlook.com>
2024-04-12 14:20:49 +1000

    Typewriter added to heropage. Completed

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,25 +1,34 @@
-import React from 'react'
-import "./intro.scss"
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed:60,
+      strings: ["Web Developer", "UX/UI Designer", "Digital Interaction Specialist", "Digital Artist"],
+    });
+  }, []);
 
   return (
     <div className="intro" id="intro">
       <div className="left">
         <div className="imgContainer">
-          <img src="\assests\me_code.png" alt="" />
-       
+        <img src="\assests\me_code.png" alt="" />
         </div>
       </div>
       <div className="right">
         <div className="wrapper">
           <h2>Hi There, I'm</h2>
-          <h1>Chloe O.</h1>
-          <h3>Freelance Web Designer<span></span></h3>
+          <h1> Chloe O.</h1>
+          <h3>Freelance <span ref={textRef}></span></h3>
         </div>
-        <a href='assests/down.png' alt=""/>
         <a href="#portfolio">
-          <img src="assets/down.png" alt="" />
+	        <img src="\assests\down.png" alt="" />
         </a>
       </div>
     </div>
