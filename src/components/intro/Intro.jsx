b113e90ffbf233f559f524ffe62601841da1690c
22415327-scu <chloeprofessionaldevelopment@outlook.com>
2024-04-12 10:49:23 +1000

    Updates to topbar location and hero image

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,14 +2,25 @@ import React from 'react'
 import "./intro.scss"
 
 export default function Intro() {
+
   return (
-    <div className="intro" id="intro"> 
-        <div className="left">
-          <div className="imgContainer"></div>
-          <img src="assests/me_code.png" alt="Cartoon version of me" />
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="\assests\me_code.png" alt="" />
+       
         </div>
-        <div className="right"></div>
-        
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Safak Kocaoglu</h1>
+          
         </div>
-  )
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
